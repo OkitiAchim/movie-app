ac636aefc9bf0f46b96723a95cf5b728b8f863db
Vwegba Okiti <ovwegbaokiti@gmail.com>
2026-03-08 08:08:01 -0700

    fix case sensitive importsfor vercel build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import "./css/App.css";
 import MovieCard from "./components/movieCard";
 import Favourites from "./pages/Favourites";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import { MovieProvider } from "./context/MovieContext";
@@ -16,7 +16,7 @@ function App() {
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Favourites" element={<Favourite />} />
+          <Route path="/Favourites" element={<Favourites />} />
           <Route path="/movie/:id" element={<MovieDetails />} />
         </Routes>
       </main>
